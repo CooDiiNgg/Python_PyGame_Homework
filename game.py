ccc1427ae192911dea92d3c1d97fcb9f623f0717
CooDDiiNNgg <niki@valkanovi.com>
2023-05-28 12:30:04 +0300

    Change_of_idea

--- game.py
+++ game.py
@@ -4,89 +4,59 @@ import random
 pygame.init()
 
 
-class Game():
+# Createing tower defense game
+
+class TowerDefence:
     def __init__(self):
-        self.screen = None
-        self.background = None
-        self.playerImg = None
-        self.playerX = 30
-        self.playerY = 400
-        self.playerX_change = 0
-        self.playerY_change = 0
-        self.bossImg = None
-        self.bossX = None
-        self.bossY = None
-        self.bossX_change = None
-        self.bossY_change = None
-        self.bulletImg = None
-        self.bulletX = None
-        self.bulletY = None
-        self.bulletX_change = None
-        self.bulletY_change = None
+        # self.map = map
+        # self.hero = hero
+        # self.enemies = enemies
+        # self.towers = towers
+        self.set_map()
+        # self.set_hero_start_place(map)
+        # self.set_tower_build_places()
     
-
-    def Create_Screen(self):
-        self.screen = pygame.display.set_mode((800, 600))
-        pygame.display.set_caption("Boss Fight Plane Game")
-        # self.background = pygame.image.load("/Images/background.png")
-        # for now it will be a basic color
-        self.background = (0, 0, 0)
-
-    def Create_Player(self):
-        # self.playerImg = pygame.image.load("/Images/player.png")
-        # For now, I will use a rectangle for the player
-        self.playerImg = pygame.draw.rect(self.screen, (255, 255, 255), (self.playerX, self.playerY, 64, 64))
-        self.playerX_change = 0
-        self.playerY_change = 0
-
-    def Create_Boss(self):
-        # self.bossImg = pygame.image.load("/Images/boss.png")
-        # For now, I will use a rectangle for the boss
-        self.bossX = 680
-        self.bossY = 400
-        self.bossImg = pygame.draw.rect(self.screen, (255, 255, 255), (self.bossX, self.bossY, 180, 180))
-        self.bossX_change = 0
-        self.bossY_change = 0
+    def set_map(self):
+        #set the background
+        self.display = pygame.display.set_mode((600, 800))
+        self.map = pygame.image.load("Images/Map.png").convert()
+        self.map = pygame.transform.scale(self.map, (600, 800))
     
-    def Create_Bullet(self):
-        # self.bulletImg = pygame.image.load("/Images/bullet.png")
-        # For now, I will use a rectangle for the bullet
-        self.bulletX = 700
-        self.bulletY = random.randint(0, 535)
-        self.bulletImg = pygame.draw.rect(self.screen, (255, 255, 255), (self.bulletX, self.bulletY, 32, 32))
-        self.bulletX_change = -10
-        self.bulletY_change = 0
-    
-    
+    def display_map(self):
+        self.display.blit(self.map, (0, 0))
+        pygame.display.update()
 
-def main():
-    game = Game()
-    game.Create_Screen()
+    # def set_hero_start_place(self, map):
+    #     self.hero_x = 0
+    #     self.hero_y = 0
+    #     for y in map:
+    #         for x in y:
+    #             if x == "S":
+    #                 self.hero_x = map.index(y)
+    #                 self.hero_y = y.index(x)
+    #                 self.hero = pygame.image.load("Images/Hero_1.png")
+    #                 self.hero.set_position(self.hero_x, self.hero_y)
+    #                 break
+    
+    # def set_tower_build_places(self):
+    #     # for now static than will be random
+    #     self.tower_x = 12
+    #     self.tower_y = 12
+        
 
+
+
+
+def test_functions():
     running = True
+    game = TowerDefence()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_a]:
-                game.playerX_change = -5
-            if keys[pygame.K_d]:
-                game.playerX_change = 5
-            if keys[pygame.K_w]:
-                game.playerY_change = -5
-            if keys[pygame.K_s]:
-                game.playerY_change = 5
-            
-            game.playerX += game.playerX_change
-            game.playerY += game.playerY_change
-            game.Create_Player()
-            game.Create_Boss()
-            pygame.display.update()
-            game.screen.fill(game.background)
+        game.display_map()
+    
+    pygame.quit()
 
 
-
-if __name__ == "__main__":
-    main()
+test_functions()
---
+++ test.py
@@ -0,0 +1,33 @@
+import pygame
+import numpy as np
+
+
+def map_to_array():
+    dict_for_map = {}
+
+    image_path = "Images/Map.png"
+    image = pygame.image.load(image_path)
+    width, height = image.get_width(), image.get_height()
+
+    surface = pygame.Surface((width, height))
+    surface.blit(image, (0, 0))
+    data = pygame.surfarray.array3d(surface)
+    data = np.swapaxes(data, 0, 1)
+
+
+    array = np.zeros((height, width), dtype=int)
+    x = 0
+    for row in range(height):
+        for col in range(width):
+            pixel = tuple(data[row, col])
+            if pixel in dict_for_map:
+                array[row, col] = dict_for_map[pixel]
+            else:
+                dict_for_map[pixel] = x
+                array[row, col] = x
+                x += 1
+
+    # save the whole array in file txt
+    np.savetxt("map.txt", array, fmt="%d")
+
+
